909d0157a23d19c7da3470356b824c0a3125e7e0
Matt Lander <matthew.lander@emperor.works>
2024-11-26 00:41:10 +0000

    experiments in movement

--- src/movement.rs
+++ src/movement.rs
@@ -12,12 +12,38 @@ impl Velocity{
   }
 }
 
+
+#[derive(Component)]
+pub struct TargetVelocity{
+  pub value: Vec3,
+}
+
+impl TargetVelocity{
+  pub fn new(value: Vec3) -> Self{
+    Self{ value }
+  }
+}
+
 #[derive(Component)]
 pub struct MaxSpeed{
   pub value:f32,
 }
 
 impl MaxSpeed{
+  pub fn new(value:f32) -> Self{
+    Self{ value }
+  }
+}
+
+
+
+
+#[derive(Component)]
+pub struct MaxLinearAcceleration{
+  pub value:f32
+}
+
+impl MaxLinearAcceleration{
   pub fn new(value:f32) -> Self{
     Self{ value }
   }
@@ -48,15 +74,40 @@ pub struct AcceleratingObjectBundle{
   pub max_speed:MaxSpeed,
 }
 
+#[derive(Bundle)]
+pub struct TargetVelocityObjectBundle{
+  pub velocity:Velocity,
+  pub target_volcity:TargetVelocity,
+  pub max_accelleration:MaxLinearAcceleration,
+  pub model: SceneBundle,
+}
+
 
 
 pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin{
     fn build(&self, app: &mut App) {
-      app.add_systems(Update, (update_velocity, update_position).chain());
+      app.add_systems(Update, (update_velocity, update_target_velocity, update_position).chain());
     }
 }
+
+
+fn update_target_velocity(mut query:Query<(&MaxLinearAcceleration, &TargetVelocity, &mut Velocity)>, time: Res<Time>){
+  for (max_accelleration, target_velocity, mut velocity) in query.iter_mut(){
+    let diff =  target_velocity.value - velocity.value;
+    if diff == Vec3::ZERO || diff.try_normalize() == None{
+      velocity.value = target_velocity.value;
+      return;
+    }
+    velocity.value = velocity.value + (diff * max_accelleration.value * time.delta_seconds());
+    if (velocity.value.length_squared() > target_velocity.value.length_squared()){
+      velocity.value = target_velocity.value;
+    }
+  }
+}
+
+
 
 fn update_velocity(mut query:Query<(&Acceleration, &MaxSpeed, &mut Velocity)>, time: Res<Time>){
   for (acceleraation, max_speed, mut velocity) in query.iter_mut(){
--- src/ship.rs
+++ src/ship.rs
@@ -1,14 +1,11 @@
 use std::f32::consts::PI;
-
 use bevy::{math::VectorSpace, prelude::*};
 
-use crate::{asset_loader::SceneAssets, movement::{AcceleratingObjectBundle, Acceleration, MaxSpeed, Velocity}};
-
-
+use crate::{asset_loader::SceneAssets, movement::{ MaxLinearAcceleration,  TargetVelocity, TargetVelocityObjectBundle, Velocity}};
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0,0.0, 0.0);
-const ACCELERATION: f32 = 500.0;
-const DECCELERATION: f32 = 250.0;
+const MAX_ACCELERATION: f32 = 5.0;
+const TARGET_SPEED: f32 = 40.0;
 
 
 #[derive(Component)]
@@ -37,37 +34,33 @@ impl Plugin for ShipPlugin{
 }
 
 
-fn movement_controls(mut query: Query<(&mut Acceleration, &mut Velocity, &mut Pitch), With<Ship>>, keyboard_input:Res<ButtonInput<KeyCode>>, time: Res<Time>){
-  let Ok((mut acc, mut velocity, mut pitch)) = query.get_single_mut() else{
+fn movement_controls(mut query: Query<(&mut TargetVelocity, &mut Pitch), With<Ship>>, keyboard_input:Res<ButtonInput<KeyCode>>, time: Res<Time>){
+  let Ok((mut target_velocity, mut pitch)) = query.get_single_mut() else{
     return;
   };
-  acc.value = Vec3::ZERO;
+
+
+  let mut direction = Vec3::ZERO;
+
+  target_velocity.value = Vec3::ZERO;
   if keyboard_input.pressed(KeyCode::KeyD){
-    acc.value.x -= ACCELERATION;
+    direction.x -= 1.0;
+    //target_velocity.value.x -= TARGET_SPEED;
   }
   if keyboard_input.pressed(KeyCode::KeyA){
-    acc.value.x += ACCELERATION;
+    direction.x += 1.0;
+    //target_velocity.value.x += TARGET_SPEED;
   }  
 
   if keyboard_input.pressed(KeyCode::KeyW){
-    acc.value.z += ACCELERATION;
+    direction.z += 1.0;
+    //target_velocity.value.z += TARGET_SPEED;
   }
   if keyboard_input.pressed(KeyCode::KeyS){
-    acc.value.z -= ACCELERATION;
+    direction.z -= 1.0;
+    //target_velocity.value.z -= TARGET_SPEED;
   }
-
-
   
-  if acc.value == Vec3::ZERO{
-
-    if velocity.value.length_squared() < 100.0{
-      velocity.value = Vec3::ZERO;
-    }
-    let vel_norm = velocity.value.normalize_or_zero();
-    acc.value = vel_norm * -DECCELERATION;
-  }
-  //max_speed.value = if  acc.value == Vec3::ZERO { 0.0 } else { 100.0};
-
 
 }
 
@@ -76,15 +69,15 @@ fn spawn_ship(mut commands:Commands, scene_assets:Res<SceneAssets>){
   translation.rotate_x(PI *0.5);
   translation.rotate_z(PI *0.5);
   commands.spawn((
-    AcceleratingObjectBundle{
-      acceleration: Acceleration::new(Vec3::ZERO),
+    TargetVelocityObjectBundle{
       velocity: Velocity::new(Vec3::ZERO),
       model:     SceneBundle{ 
         scene:scene_assets.ship.clone(),
         transform:translation,
         ..default()
       },
-      max_speed: MaxSpeed::new(50.0),
+      target_volcity: TargetVelocity::new(Vec3::ZERO),
+      max_accelleration: MaxLinearAcceleration::new(MAX_ACCELERATION),
     },
     Pitch::new(0.0),
     Ship,
