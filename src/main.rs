edd8ab97b8e0ac3e658ece3bd17236837869d07d
Matt Lander <matthew.lander@emperor.works>
2025-02-14 00:31:50 +0000

    windows messing about, trying to fix wasm touch issue

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ mod sidewinder;
 mod state;
 
 use asset_loader::AssetLoaderPlugin;
-use bevy::{asset::AssetMetaCheck, prelude::*};
+use bevy::{asset::AssetMetaCheck, core::FrameCount, prelude::*};
 use bounds_check::BoundsCheckPlugin;
 use bullet::BulletPlugin;
 use camera::CameraPlugin;
@@ -30,7 +30,11 @@ use movement::MovementPlugin;
 use scheduling::SchedulingPlugin;
 use ship::ShipPlugin;
 use sidewinder::SidewinderPlugin;
-use state::StatePlugin;
+use state::{GameState, StatePlugin};
+
+
+const APP_NAME:&str = "The Claw 2";
+const CANVAS_ID:&str = "game_canvas";
 
 fn main() {
   App::new()
@@ -39,11 +43,26 @@ fn main() {
       color: Color::default(),
       brightness: 750.0,
     })
+    .add_plugins(
+      DefaultPlugins
+        .set( WindowPlugin{
+          primary_window: Some(Window{
+            title: APP_NAME.into(),
+            name: Some(APP_NAME.into()),
+            canvas: Some(CANVAS_ID.into()),
+            fit_canvas_to_parent: true,
+            visible: false,
+            ..default()
+          }),
+          ..default()
+        })
+        .set(AssetPlugin {
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        }
+      )
+    )
     .add_plugins((
-      DefaultPlugins.set(AssetPlugin {
-        meta_check: AssetMetaCheck::Never,
-        ..Default::default()
-      }),
       StatePlugin,
       SchedulingPlugin,
       CameraPlugin,
@@ -60,5 +79,16 @@ fn main() {
       HookPlugin,
     ))
     .add_plugins((GameInputPlugin,))
+    .add_systems(Update, make_visible.run_if(in_state(GameState::Loading)))
     .run();
+}
+
+
+fn make_visible(mut window:Single<&mut Window>, frames:Res<FrameCount>){
+
+  info!("frame {:?}", frames.0);
+  if frames.0 == 1{
+    window.visible = true;
+  }
+
 }
