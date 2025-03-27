adbe65656c278d2852680f6fc860bb014aa5488c
Matt Lander <matthew.lander@emperor.works>
2025-03-27 01:12:52 +0000

    work in progress config parsing

--- src/ai.rs
+++ src/ai.rs
@@ -1,5 +1,8 @@
-use bevy::prelude::*;
+use std::str::FromStr;
+
+use bevy::{prelude::*, utils::HashMap};
 use bevy_common_assets::json::JsonAssetPlugin;
+use strum_macros::EnumString;
 
 use crate::{asset_loader::AssetsLoading, state::GameState};
 pub struct AiPlugin;
@@ -8,13 +11,31 @@ impl Plugin for AiPlugin{
   fn build(&self, app: &mut App) {
     app
       .add_plugins(JsonAssetPlugin::<AiConfig>::new(&["aiconfig.json"]),)
-      .init_resource::<AiData>()
+      .init_resource::<AiDataCollection>()
       .add_systems(Startup, load_configs)
       .add_systems(OnExit(GameState::Loading), parse_configs);
     //app
     //.init_resource::<BehaviourCollection>();
   }
 }
+
+
+
+#[derive(EnumString, PartialEq)]
+enum Action{
+  Idle,
+  SineWave{
+    period:f32,
+  }, 
+  Turn{
+    time:f32,
+  },
+  Home{
+    max_speed:f32,
+  }
+}
+
+
 
 #[derive(Component)]
 pub struct AiRegister{
@@ -30,66 +51,104 @@ impl AiRegister{
 
 
 fn load_configs(
+  mut commands:Commands,
   asset_server:Res<AssetServer>,
   mut loading:ResMut<AssetsLoading>,
-  mut ai_data:ResMut<AiData>,
   query:Query<&mut AiRegister>,
 ){
+
+
+  let mut ai_configs = AiConfigCollection::default();
   info!("Loading configs");
   for (i, AiRegister{name, mut index}) in query.iter().enumerate(){
     let path = format!("data/ai/{}.aiconfig.json",name);
-
     info!("Loading {:?}", path);
     let config:Handle<AiConfig> = asset_server.load(path);
     loading.0.push(config.clone().untyped());
     index = Some(i);
-    ai_data.config_handles.push(config);
+    ai_configs.0.push(config);
   }
+  commands.insert_resource(ai_configs);
 }
 
 
 fn parse_configs(
-  mut ai_data:ResMut<AiData>,
+  mut commands:Commands,
+  ai_configs:Res<AiConfigCollection>,
+  mut ai_data_collection:ResMut<AiDataCollection>,
   config_assets:Res<Assets<AiConfig>>,
-
 ){
-  for handle in ai_data.config_handles.iter(){
+  for handle in ai_configs.0.iter(){
     if let Some(config) = config_assets.get(handle.id()){
+      //new ai data!
+      let mut ai_data = AiData::default();
 
-      for action in config.actions.iter(){
-        info!(action.name);
-
+      //build hashmap of indexes
+      for (index, action_config) in config.actions.iter().enumerate(){
+        ai_data.action_indexes.insert(action_config.name.clone(), index);
+        ai_data.actions.push(build_action(action_config));
+        info!(action_config.name);
+      }
+      for (index, behaviour) in config.behaviour.iter().enumerate(){
+        ai_data.action_indexes.insert(behaviour.name.clone(), index);
+        info!(behaviour.name);
       }
 
+      //store our new data
+      ai_data_collection.0.push(ai_data); 
     }
-
-
   }
+  //dont need the configs anymore
+  commands.remove_resource::<AiConfigCollection>();
+}
 
+fn build_action(action_config: &ActionConfig) -> Action {
+
+  if let Ok(result) = Action::from_str(action_config.action_name.as_str()){
+    match (result) {
+        Action::Idle => Action::Idle,
+        Action::SineWave { period: _ } =>  Action::SineWave { period:1.1 },
+        Action::Turn { time: _ } => Action::Turn { time: 1.2 },
+        Action::Home { max_speed: _ } => Action::Home { max_speed: 20.0 },
+    }
+  }
+  else{
+    panic!("failed parsing action {:?}",action_config.action_name);
+  }
 }
 
 
-#[derive(Resource)]
-struct AiConfigHandle();
+#[derive(Resource, Default)]
+pub struct AiDataCollection(Vec<AiData>);
 
 #[derive(Resource, Default)]
 pub struct AiData{
-  config_handles:Vec<Handle<AiConfig>>
+  behaviour_indexes:HashMap<String, usize>,
+  action_indexes:HashMap<String, usize>,
+  actions:Vec<Action>,
+
 }   
+
+
+
+#[derive(Resource, Default)]
+pub struct AiConfigCollection(Vec<Handle<AiConfig>>);
+
 
 #[derive(serde::Deserialize, Asset, TypePath)]
 struct AiConfig {
-  actions: Vec<Action>,
-  behaviour:Vec<Behaviour>,
+  actions: Vec<ActionConfig>,
+  behaviour:Vec<BehaviourConfig>,
 }
 
 #[derive(serde::Deserialize)]
-struct Action{
+struct ActionConfig{
   name:String,
+  action_name:String,
 }
 
 #[derive(serde::Deserialize)]
-struct Behaviour{
+struct BehaviourConfig{
   name:String,
 }
 
